57eefc08a6db557bb8a25d2092c06d55c932fd52
ankit <ankit.sanghavi87@gmail.com>
2021-10-11 19:37:34 +0530

    project init

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    type Book{
+        title:String,
+        author:String
+    }
+
+    type Query{
+        books:[Book]
+    }
+`
+
+const books = [
+    {
+        title:"Book 1",
+        author:"Author 1",
+    },
+    {
+        title:"Book 2",
+        author:"Author 2",  
+    },
+]
+
+const resolvers = {
+    Query: {
+        books:()=>books
+    }
+}
+
+const server =new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({url}) => {
+    console.log(`Server read at ${url}`)
+})
