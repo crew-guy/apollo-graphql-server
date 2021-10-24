5a64b15044b8dcaf6c50ad56bf48a85074b22359
ankit <ankit.sanghavi87@gmail.com>
2021-10-24 20:19:08 +0530

    useQuery

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,27 @@ import { Switch, Route, BrowserRouter } from "react-router-dom"
 import LandingPage from './pages/LandingPage';
 import CategoryPage from './pages/CategoryPage';
 import AnimalPage from './pages/AnimalPage/AnimalPage';
+import {InMemoryCache, ApolloClient, ApolloProvider} from '@apollo/client'
 
 function App() {
+
+  const client = new ApolloClient({
+    uri: "http://localhost:4000",
+    cache : new InMemoryCache()
+  })
+
   return (
     <div className="App">
-      <BrowserRouter>
-          <Nav />
-          <Switch>
-            <Route exact strict path="/" component={LandingPage}/>
-            <Route exact strict path="/products/:slug" component={CategoryPage}/>
-            <Route exact strict path="/product/:slug" component={AnimalPage}/>
-          </Switch>
-      </BrowserRouter>
+      <ApolloProvider client={client} >
+        <BrowserRouter>
+            <Nav />
+            <Switch>
+              <Route exact strict path="/" component={LandingPage}/>
+              <Route exact strict path="/products/:slug" component={CategoryPage}/>
+              <Route exact strict path="/product/:slug" component={AnimalPage}/>
+            </Switch>
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
   );
 }
--- client/src/components/MainHero/MainHero.js
+++ client/src/components/MainHero/MainHero.js
@@ -2,22 +2,35 @@ import React from 'react'
 import "./MainHero.css"
 import animals from "../../assets/images"
 import {Container} from "reactstrap"
+import {useQuery, gql} from '@apollo/client'
 
 function MainHero() {
 
+    const { loading, error, data } = useQuery(gql`
+        {
+            mainCards {
+                title
+                image
+            }
+        }
+    `)
+    
+    if (loading) return <div>Loading.....</div>
+    if(error) return <div>An error has occurred..</div>
+
     return (
         <div className="MainHero">
-            <Container>
+            <Container> 
                     <div className="header-container">
                         <h2>Find your <br/> new four-legged <br/> best friend</h2>
                         <img src={animals.rhino} />
                     </div>
                     <div className="cards-container">
-                        {[].map(card => {
+                        {data.mainCards.map(card => {
                             return (
                                 <div className="card">
                                     <h3>{card.title}</h3>
-                                    <img src={animals[card.img]} style={{width: "100%"}}/>
+                                    <img src={animals[card.image]} style={{width: "100%"}}/>
                                 </div>
                             )
                         })}
