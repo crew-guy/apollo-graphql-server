ba7334dd07ba68ad1db79493c23aea7a3d9bd8b2
ankit <ankit.sanghavi87@gmail.com>
2021-10-24 20:22:18 +0530

    animals cards galleruy

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -2,14 +2,33 @@ import React from 'react'
 import MainHero from "../components/MainHero/MainHero"
 import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay"
 import CardDisplay from "../components/CardDisplay/CardDisplay"
+import { useQuery, gql } from '@apollo/client'
 
 function LandingPage() {
 
+    const FETCH_ANIMALS = gql`
+        {
+            animals{
+                image,
+                price,
+                id,
+                title,
+                slug,
+                stock
+            }
+        }
+    `
+
+    const { loading, error, data } = useQuery(FETCH_ANIMALS)
+    
+    if (loading) return <div>Loading</div>
+    if (error) return <div>error</div>
+    
     return (
         <div>
             <MainHero />
             <CategoryDisplay />
-            <CardDisplay animals={[]}/>
+            <CardDisplay animals={data.animals}/>
         </div>
     )
 }
